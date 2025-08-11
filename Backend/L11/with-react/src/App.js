a5197424ade8afe7144a5006195a9179b59e07e3
knoxiboy <karankmt.tripathi@gmail.com>
2025-08-11 16:10:49 +0530

    11-08-2025

---
+++ Backend/L11/with-react/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <div className="value">
+        {count}
+      </div>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
